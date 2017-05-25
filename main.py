bf489fd71c16401461a8fdb56bc491024cd3ccb9
rajsimran1996@gmail.com <Your Email Id>
2017-05-25 11:29:53 +0530

    Second Commit -Spy has a personality

--- main.py
+++ main.py
@@ -23,7 +23,41 @@ if spy_name_length > 0:
     spy_name = spy_salutation + " " + spy_name
     print "Alright, " + spy_name + " We need some more information."
 
+    # declare new variables
+    spy_age = 0
+    spy_rating = 0.0
+    spy_is_online = False
 
+    # Ask for the age of the spy
+    spy_age = raw_input("please enter your age: ")
+    # raw input always gives a string to typecast age to int
+    spy_age = int(spy_age)
+
+    # Age cannot be less than 12 and greater than 50
+    # nested if
+    if 12 <= spy_age < 50:
+        print("Welcome to Spy community")
+
+        # Ask for spy_rating
+        spy_rating = float(raw_input("Please enter your spy rating: "))
+
+        if spy_rating > 4.5:
+            print "Great Ace!"
+        elif 3.5 <= spy_rating <=4.5:
+            print "You are one of the good ones!"
+        elif 2.5 <= spy_rating <=3.5:
+            print "You can always do better."
+        else:
+            print "We will get someone to help you."
+
+        # Make the spy come online
+        spy_is_online = True
+
+        # Authentication Complete
+        print "Authentication Complete.Welcome %s,age: %d,rating : %.2f.Proud to have you on board." %(spy_name,spy_age,spy_rating)
+
+    else:
+        print("You are not of the proper age to enter the spy community!")
 
 else:
     print("Please enter your name and run the program again.")
